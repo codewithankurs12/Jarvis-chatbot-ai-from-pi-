5de42e4bd2eb3a022de299721ce217ffbd7164b6
Ankur Sharma <168324277+codewithankurs12@users.noreply.github.com>
2024-12-24 21:04:03 +0530

    Update Friday.py

--- Friday.py
+++ Friday.py
@@ -1,6 +1,12 @@
 import threading
 
 from logic_brain import friday_brain
+import eel
+
+
+def ui():
+    eel.start("index.html",mode='chrome', port=6756, cmdline_args=['--start-fullscreen'])
+
 
 def friday():
     t1 = threading.Thread(target=friday_brain)
